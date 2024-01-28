d7b9a60d0991d5638c85fffc70ae47f9a7e444bc
Михаил Юрин <MYurin@alfabank.ru>
2024-01-29 01:44:20 +0300

    createSelector

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -3,11 +3,11 @@ import { RootState } from ".";
 
 const selfSelector = (state: RootState) => state.bookOrderReducer;
 
-export const pairSelector = (state: RootState) => createSelector(selfSelector, (state) => state.pair);
+export const pairSelector = createSelector(selfSelector, (state) => state.pair);
 
-export const bidsSelector = (state: RootState) => createSelector(selfSelector, (state) => state.bids);
+export const bidsSelector = createSelector(selfSelector, (state) => state.bids);
 
-export const asksSelector = (state: RootState) => createSelector(selfSelector, (state) => state.asks);
+export const asksSelector = createSelector(selfSelector, (state) => state.asks);
 
 export const connectionSelector = createSelector(selfSelector, (state) => state.connection);
 
